e8ab41e94a6d55ee7d20c07b2f45a1912bab89fd
Edwin Zamudio <ezamudio@hawk.iit.edu>
2023-07-26 01:15:11 -0500

    feat: persistence
    Add Serde to handle (de)serialization, remove page code for now, store bytes in a file

--- src/main.rs
+++ src/main.rs
@@ -1,130 +1,85 @@
-use rustyline::{error::ReadlineError, Config, Editor, Result};
-use std::result as std_result;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{Read, Write};
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
 
-const SIZE_OF_ID: usize = std::mem::size_of::<u32>();
-const PAGE_SIZE: usize = 4096;
-const TABLE_MAX_PAGES: usize = 100;
-const ROWS_PER_PAGE: usize = PAGE_SIZE / SIZE_OF_ID;
-const TABLE_MAX_ROWS: usize = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+const TABLE_MAX_ROWS: usize = 100;
 
 enum StatementType {
     Insert,
     Select,
+    Truncate
 }
 
 struct Statement {
     type_: StatementType,
     row_insert: Row,
 }
-#[derive(Clone, Default, Debug)]
+#[derive(Default, Serialize, Deserialize)]
 struct Row {
     id: u32,
     username: String,
     email: String,
 }
 
+#[derive(Default, Serialize, Deserialize)]
 struct Table {
+    name: String,
     num_rows: usize,
-    pages: Vec<Page>,
-    // pager: Pager,
-}
-
-impl Table {
-    // fn new(filename: &str) -> Self {
-    fn new() -> Self {
-        Table {
-            num_rows: 0,
-            pages: vec![],
-            // pager: open_pager(filename).unwrap(),
-        }
-    }
-}
-
-#[derive(Clone)]
-struct Page {
     rows: Vec<Row>,
 }
 
-impl Page {
-    fn new() -> Self {
-        Page { rows: vec![] }
+impl Table {
+    fn new(name: Option<String>) -> Table {
+        return Table {
+            name: name.unwrap_or("fct".to_owned()),
+            num_rows: 0,
+            rows: vec![],
+        };
+    }
+
+    fn open_file(filename: &str) -> Result<File, String> {
+        let file = std::fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(filename)
+            .map_err(|e| e.to_string())?;
+        Ok(file)
+    }
+
+    fn db_close(&mut self) -> Result<(), String> {
+        let serialized_bytes = bincode::serialize(&self).expect("Serialization failed");
+        let mut file = Table::open_file(&self.name).unwrap();
+        file.write_all(&serialized_bytes)
+            .map_err(|e| e.to_string())?;
+        Ok(())
     }
 }
 
-// struct Pager {
-//     file: std::fs::File,
-//     file_length: usize,
-//     pages: Vec<Page>,
-// }
+fn db_open(filename: String) -> Result<Table, String> {
+    let mut file = match Table::open_file(&filename).map_err(|e| e.to_string()) {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
 
+    let mut buffer = vec![];
+    if let Err(e) = file.read_to_end(&mut buffer) {
+        return Err(e.to_string());
+    }
 
-// fn open_pager(filename: &str) -> std_result::Result<Pager, String> {
-//     let file = std::fs::OpenOptions::new()
-//         .read(true)
-//         .write(true)
-//         .create(true)
-//         .open(filename)
-//         .map_err(|e| e.to_string())?;
-//     let file_length = file
-//         .metadata()
-//         .map_err(|e| e.to_string())?
-//         .len() as usize;
-//     let pages = (0..TABLE_MAX_PAGES).map(|_| Page::new()).collect();
-//     Ok(Pager {
-//         file,
-//         file_length,
-//         pages,
-//     })
-// }
+    if buffer.is_empty() {
+        return Ok(Table::new(None));
+    }
 
-
-
-fn table_row_slot(table: &Table, row_num: usize) -> &Row {
-    let page_num = row_num / ROWS_PER_PAGE;
-    let row_offset = row_num % ROWS_PER_PAGE;
-    &table.pages[page_num].rows[row_offset]
-}
-
-// serialize row using memcpy
-fn serialize_row(source: &Row, destination: &mut Vec<u8>) {
-    let id_bytes = source.id.to_le_bytes();
-    let username_bytes = source.username.as_bytes();
-    let email_bytes = source.email.as_bytes();
-    destination.extend_from_slice(&id_bytes);
-    destination.extend_from_slice(username_bytes);
-    destination.extend_from_slice(email_bytes);
-}
-
-// fn deserialize_row(source: &[u8]) -> Row {
-//     let id = u32::from_le_bytes([source[0], source[1], source[2], source[3]]);
-//     let username_start = SIZE_OF_ID;
-//     let username_end = username_start + source[username_start..]
-//         .iter()
-//         .position(|&x| x == 0)
-//         .unwrap();
-//     let username = String::from_utf8(source[username_start..username_end].to_vec()).unwrap();
-//     let email_start = username_end + 1;
-//     let email_end = email_start + source[email_start..]
-//         .iter()
-//         .position(|&x| x == 0)
-//         .unwrap();
-//     let email = String::from_utf8(source[email_start..email_end].to_vec()).unwrap();
-//     Row {
-//         id,
-//         username,
-//         email,
-//     }
-// }
-
-fn do_meta_command(input: &str) -> std_result::Result<(), String> {
-    if input == ".exit" {
-        std::process::exit(0);
-    } else {
-        Err(format!("Unrecognized command '{}'.", input))
+    match bincode::deserialize(&buffer) {
+        Ok(deserialized_table) => Ok(deserialized_table),
+        Err(e) => Err(format!("Deserialization failed: {}", e)),
     }
 }
 
-fn prepare_statement(input: &str) -> std_result::Result<Statement, String> {
+fn prepare_statement(input: &str) -> Result<Statement, String> {
     let mut iter = input.split_whitespace();
     if let Some(command) = iter.next() {
         match command {
@@ -155,6 +110,12 @@ fn prepare_statement(input: &str) -> std_result::Result<Statement, String> {
                     row_insert: Row::default(),
                 });
             }
+            "truncate" => {
+                return Ok(Statement {
+                    type_: StatementType::Truncate,
+                    row_insert: Row::default(),
+                });
+            }
             _ => {
                 return Err(format!("Unrecognized keyword at start of '{}'.", input));
             }
@@ -163,41 +124,41 @@ fn prepare_statement(input: &str) -> std_result::Result<Statement, String> {
     return Err(format!("Unrecognized input '{}'.", input));
 }
 
-fn execute_statement(statement: Statement, table: &mut Table) -> std_result::Result<(), String> {
+fn execute_statement(statement: Statement, table: &mut Table) -> Result<(), String> {
     match statement.type_ {
         StatementType::Insert => {
             if table.num_rows >= TABLE_MAX_ROWS {
                 return Err("Table full.".to_owned());
             }
             let row_to_insert = statement.row_insert;
-            let mut destination =
-                vec![0u8; SIZE_OF_ID + row_to_insert.username.len() + row_to_insert.email.len()];
-            serialize_row(&row_to_insert, &mut destination);
             let row_num = table.num_rows;
-            let page_num = row_num / ROWS_PER_PAGE;
-            let row_offset = row_num % ROWS_PER_PAGE;
-
-            if page_num >= table.pages.len() {
-                table.pages.push(Page::new());
-            }
-
-            table.pages[page_num].rows.insert(row_offset, row_to_insert);
+            table.rows.insert(row_num, row_to_insert);
             table.num_rows += 1;
             return Ok(());
         }
         StatementType::Select => {
             for i in 0..table.num_rows {
-                let row = table_row_slot(table, i);
-                println!("{:?}", row);
+                let row = &table.rows[i];
+                println!("({}, {}, {})", row.id, row.username, row.email);
             }
+            Ok(())
+        }
+        StatementType::Truncate => {
+            table.rows.clear();
+            table.num_rows = 0;
             Ok(())
         }
     }
 }
 
-fn handle_valid_input(input: String, table: &mut Table) -> std::result::Result<(), String> {
+fn handle_valid_input(input: String, table: &mut Table) -> Result<(), String> {
     if input.starts_with(".") {
-        return do_meta_command(&input);
+        if input == ".exit" {
+            table.db_close().unwrap();
+            std::process::exit(0);
+        } else {
+            return Err(format!("Unrecognized command '{}'.", input));
+        }
     }
 
     match prepare_statement(&input) {
@@ -206,25 +167,17 @@ fn handle_valid_input(input: String, table: &mut Table) -> std::result::Result<(
     }
 }
 
-fn main() -> Result<()> {
-    let mut table = Table::new();
-
-    let config = Config::builder().auto_add_history(true).build();
-    let history = rustyline::sqlite_history::SQLiteHistory::open(config, "history.sqlite3")?;
-
-    let mut repl_: Editor<(), _> = Editor::with_history(config, history).unwrap();
-
+fn main() {
+    let mut table = db_open("fct".to_owned()).unwrap();
+    // don't worry about history yet
+    let mut repl_ = DefaultEditor::new().expect("can't open REPL");
     loop {
         let readline = repl_.readline("db > ");
-
         match readline {
-            Ok(input) => {
-                repl_.add_history_entry(input.as_str())?;
-                match handle_valid_input(input, &mut table) {
-                    Ok(_) => println!("Command executed."),
-                    Err(e) => println!("Error: {}", e),
-                }
-            }
+            Ok(input) => match handle_valid_input(input, &mut table) {
+                Ok(_) => println!("Command executed."),
+                Err(e) => println!("Error: {}", e),
+            },
             Err(ReadlineError::Interrupted) => {
                 println!("CTRL-C");
                 break;
@@ -235,7 +188,6 @@ fn main() -> Result<()> {
             }
         }
     }
-    Ok(())
 }
 
 #[cfg(test)]
@@ -243,74 +195,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_table_row_slot() {
-        let table = Table {
-            num_rows: 0,
-            pages: vec![
-                Page {
-                    rows: vec![
-                        Row {
-                            id: 0,
-                            username: "bruh".to_owned(),
-                            email: "bruh".to_owned(),
-                        };
-                        ROWS_PER_PAGE
-                    ]
-                };
-                TABLE_MAX_PAGES
-            ],
-        };
-        let row = table_row_slot(&table, 0);
-        assert_eq!(row.id, 0);
-        assert_eq!(row.username, "bruh".to_owned());
-        assert_eq!(row.email, "bruh".to_owned());
-    }
-
-    #[test]
-    fn test_prepare_statement() {
-        let statement = prepare_statement("insert 1 test nah");
-        assert_eq!(statement.is_ok(), true);
-        let statement = prepare_statement("insert 1 test");
-        assert_eq!(statement.err().unwrap(), "Invalid input format");
-        let statement = prepare_statement("insert -1 test nah nah");
-        // rust handles negative numbers since they can't be parsed as u32
-        assert_eq!(statement.err().unwrap(), "Invalid ID format");
-
-        let statement = prepare_statement("select");
-        assert_eq!(statement.is_ok(), true);
-        let statement = prepare_statement("select 1");
-        assert_eq!(statement.is_ok(), true);
-    }
-
-    #[test]
-    fn test_execute_statement() {
-        let mut table = Table {
-            num_rows: 0,
-            pages: vec![
-                Page {
-                    rows: vec![
-                        Row {
-                            id: 0,
-                            username: "".to_owned(),
-                            email: "".to_owned(),
-                        };
-                        ROWS_PER_PAGE
-                    ]
-                };
-                TABLE_MAX_PAGES
-            ],
-        };
+    fn test_insert() {
+        let mut table = Table::new(None);
         let statement = Statement {
             type_: StatementType::Insert,
             row_insert: Row {
                 id: 1,
-                username: "reallylongnamethatshouldfailbruhhhhhhhhh".to_owned(),
-                email: "reallylongnamethatshouldfailbruh".to_owned(),
+                username: "test".to_owned(),
+                email: "nah".to_owned(),
             },
         };
         let result = execute_statement(statement, &mut table);
-        // rust handles long strings since we don't have a max length
         assert_eq!(result.is_ok(), true);
+    }
+
+    #[test]
+    fn test_select() {
+        let mut table = Table::new(None);
         let statement = Statement {
             type_: StatementType::Select,
             row_insert: Row {
